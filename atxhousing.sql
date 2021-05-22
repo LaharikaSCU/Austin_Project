8d3ce5bfa9e14ffae3e3241b3523ace51982b4cf
shireenkahlon <75495212+shireenkahlon@users.noreply.github.com>
2021-05-21 19:44:43 -0500

    Added location and price tables to SQL

--- atxhousing.sql
+++ atxhousing.sql
@@ -46,3 +46,38 @@ create table atxhousing(
     numOfBedrooms numeric,
     numOfStories numeric )
 
+/* View the table results */
+SELECT *
+FROM atxhousing
+
+/* Create a table for locations */
+CREATE TABLE houseLocation AS
+SELECT zpid, street address, city, zipcode,  latitude, longitude
+FROM atxhousing
+
+/* Add a primary key to the locations table */
+ALTER TABLE houseLocation
+ADD PRIMARY KEY (zpid)
+
+/* View the locations table */
+SELECT *
+FROM houseLocation
+
+/* Create a table for price */
+CREATE TABLE price AS
+SELECT zpid, propertytaxrate, latest price, numpricechanges, latest_saledate, latest_salemonth, latestpricesource
+FROM atxhousing
+
+/* Add a primary key for price */
+ALTER TABLE price
+ADD PRIMARY KEY (zpid)
+
+/* Add a foreign key for price referencing the location table */
+ALTER TABLE houseLocation
+ADD FOREIGN KEY (zpid)
+REFERENCES houseLocation (zpid)
+
+/* View table */
+SELECT *
+FROM price
+
